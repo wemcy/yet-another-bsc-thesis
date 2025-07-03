8f941533c60b913fdb6c8012e68ebe3d093f1a94
WencyFyre <olti10@gmail.com>
2025-07-03 20:52:07 +0200

    Working proxy

--- src/proxy/entrypoint.sh
+++ src/proxy/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-envsubst '${PUBLIC_HOST} ${BACKEND_HOST} ${BACKEND_PORT} ${KEYCLOAK_HOST} ${KEYCLOAK_PORT} ${FRONTEND_HOST} ${FRONTEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
-cat /etc/nginx/conf.d/default.conf
+# envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf 
+# cat /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
