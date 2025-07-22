be7dd7d250bdf192d8bb61b9aa0e9ab6123c51a7
WencyFyre <olti10@gmail.com>
2025-07-22 20:53:53 +0200

    Added Login to

--- src/frontend/recipe-web-frontend/src/main.ts
+++ src/frontend/recipe-web-frontend/src/main.ts
@@ -5,9 +5,17 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { vueKeycloak } from '@josempgon/vue-keycloak'
 
 const app = createApp(App)
 
+app.use(vueKeycloak, {
+    config: {
+        url: 'http://localhost:9393/auth', //TODO make this configurable
+        realm: 'recipeapp',
+        clientId: 'recipeapp-frontend',
+    },
+})
 app.use(router)
 app.use(createPinia())
 
