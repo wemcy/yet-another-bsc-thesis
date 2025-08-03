42511c51881887da19ab4badb0989c8e34b41367
WencyFyre <olti10@gmail.com>
2025-08-03 19:35:31 +0200

    Added verison updater

--- scripts/generate_versions.ps1
+++ scripts/generate_versions.ps1
@@ -1,11 +1,24 @@
 $ErrorActionPreference = 'Stop'
 
-# Set-Location -LiteralPath $PSScriptRoot
-
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 $env:DOTNET_NOLOGO = '1'
 
+$target = [System.IO.Path]::GetFullPath((Join-Path -Path $PSScriptRoot -ChildPath "..\gitversion.env"))
+
 dotnet tool restore
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-dotnet gitversion /output dotenv > gitversion.env
+
+Write-Output $target
+dotnet tool run dotnet-gitversion /verbosity Verbose /output dotenv > $target
+dotnet tool run dotnet-gitversion /verbosity Verbose /updateprojectfiles
+
+$version = dotnet tool run dotnet-gitversion /output json /showvariable FullSemVer
+
+pnpm recursive exec -- pnpm version $version
+
+
+if ($env:CI -eq 'true') {
+    git tag $version
+    git push origin tag $version
+}
