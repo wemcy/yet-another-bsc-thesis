bcc61267a682964593b3391b3954c34a3e4ae0a1
WencyFyre <olti10@gmail.com>
2026-04-12 17:12:26 +0200

    Updated frontend path

--- src/frontend/recipe-web-frontend/src/utils/recipeApiClient.ts
+++ src/frontend/recipe-web-frontend/src/utils/recipeApiClient.ts
@@ -2,8 +2,7 @@ import { Configuration, RecipeApiClient } from 'recipe-api-client'
 
 export const recipeApiClient = new RecipeApiClient(
     new Configuration({
-        // TODO make this dynamic based on env vars
-        basePath: 'http://localhost:9393/api',
+        basePath: '/api',
         credentials: 'include',
     }),
 )
