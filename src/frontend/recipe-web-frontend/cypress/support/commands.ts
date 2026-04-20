fb47d6266d2e6f99592208444d28db9f7d256a50
WencyFyre <olti10@gmail.com>
2026-04-20 19:17:08 +0200

    Some thesis restructure.

--- src/frontend/recipe-web-frontend/cypress/support/commands.ts
+++ src/frontend/recipe-web-frontend/cypress/support/commands.ts
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace Cypress {
         interface Chainable {
             /**
