b8e9c5cb28775874056f50da2fde15bf00544c54
WencyFyre <olti10@gmail.com>
2026-05-01 15:33:45 +0200

    Fixed more things

--- build_local.ps1
+++ build_local.ps1
@@ -6,4 +6,3 @@ Set-Location .\src\frontend
 pnpm install 
 pnpm recursive run --sort build
 Pop-Location
-docker compose --env-file gitversion.env --env-file .env -f docker-compose.yaml -f docker-compose-build.yaml build --platform linux/amd64
